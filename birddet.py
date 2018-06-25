fef2d7330634427f95559b0839f42d9a6b0fe750
sidrah-liaqat <sidrah.liaqat@uky.edu>
2018-06-25 16:03:58 -0400

    Step 1 - First fold of training (train_B & val_B) with normalized input data (imagedata + 15.0966)/(15.0966 + 2.25745)

--- birddet.py
+++ birddet.py
@@ -45,7 +45,6 @@ label = np.zeros(1)
 def data_generator(filelistpath, batch_size=32, shuffle=False):
     batch_index = 0
     image_index = -1
-    print(filelistpath)
     filelist = open(filelistpath, 'r')
     filenames = filelist.readlines()
     filelist.close()
@@ -93,8 +92,10 @@ def data_generator(filelistpath, batch_size=32, shuffle=False):
             yield inputs, outputs
 
 
-train_filelist=[FILELIST+'train_B']
-val_filelist=[FILELIST+'val_B']
+#train_filelist=[FILELIST+'train_B']
+#val_filelist=[FILELIST+'val_B']
+train_filelist=['/audio/audio/workingfiles/filelists/train_B']
+val_filelist=['/audio/audio/workingfiles/filelists/val_B']
 
 train_generator = data_generator(train_filelist, 32, False)
 validation_generator = data_generator(val_filelist, 32, False)
